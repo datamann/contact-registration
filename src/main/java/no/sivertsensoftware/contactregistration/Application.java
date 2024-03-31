d358068df63c07e59cf348c23545d2a1c29ad9e5
Stig Sivertsen <sbsivertsen@gmail.con>
2024-03-31 14:24:15 +0200

    Renamed Application. Added application.yml. Temporarely removed database and security in pom.xml

--- src/main/java/no/sivertsensoftware/contactregistration/Application.java
+++ src/main/java/no/sivertsensoftware/contactregistration/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ContactRegistrationApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ContactRegistrationApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
