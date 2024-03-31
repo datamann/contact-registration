3fe708298547a409f5badd8733eada3008bb6760
Stig Sivertsen <sbsivertsen@gmail.con>
2024-03-31 14:03:22 +0200

    Added basic Spring Boot App and modified gitignore

---
+++ src/main/java/no/sivertsensoftware/contactregistration/ContactRegistrationApplication.java
@@ -0,0 +1,13 @@
+package no.sivertsensoftware.contactregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactRegistrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/no/sivertsensoftware/contactregistration/ContactRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package no.sivertsensoftware.contactregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
